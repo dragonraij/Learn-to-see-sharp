6f733504e396f7ca9451974ec5966f249e00e108
Raj Prasad <raj-prasad@live.com>
2016-05-04 18:24:03 +1200

    Add element to sets

---
+++ fundamentals/CompareHashSets/CompareHashSets/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareHashSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var bigCities = new HashSet<string>
+                (StringComparer.InvariantCultureIgnoreCase)
+            { "new york", "Manchester", "sheffield", "Paris"};
+
+            bigCities.Add("SHEFFIELD");
+            bigCities.Add("BEIJENG");
+
+            foreach (string city in bigCities)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
