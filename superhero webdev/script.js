ca7674f1bcaccb3cdb8798981ac06be5ecc49ec1
Raj Prasad <raj-prasad@live.com>
2016-05-20 23:54:30 +1200

    Change number of items returned

--- superhero webdev/script.js
+++ superhero webdev/script.js
@@ -1,7 +1,7 @@
 
 (function() {
-  var flickerAPI = "http://gateway.marvel.com:80/v1/public/characters?apikey=deefa48fe399ff4dac20626ecce6f5e5";
-  $.getJSON( flickerAPI, {
+  var characterAPI = "http://gateway.marvel.com:80/v1/public/characters?limit=5&offset="+Math.floor(Math.random()*1485)+"&apikey=deefa48fe399ff4dac20626ecce6f5e5";
+  $.getJSON( characterAPI, {
 
   })
     .done(function( data ) {
@@ -9,6 +9,24 @@
         $( "<img>" ).attr( "src", item.thumbnail.path+".jpg" ).appendTo( "#char_image" );
           $( "#char_name" ).append( item.name);
           $( "#char_bio" ).append( "<p>"+item.description+"</p>" );
+        if ( i === 0 ) {
+          return false;
+        }
+      });
+    });
+})();
+
+
+(function() {
+  var comicAPI = "http://gateway.marvel.com:80/v1/public/comics?limit=5&offset="+Math.floor(Math.random()*36893)+"&apikey=deefa48fe399ff4dac20626ecce6f5e5";
+  $.getJSON( comicAPI, {
+
+  })
+    .done(function( data ) {
+      $.each( data.data.results, function( i, item ) {
+        $( "<img>" ).attr( "src", item.thumbnail.path+".jpg" ).appendTo( "#comic_image" );
+          $( "#comic_name" ).append( item.title);
+          $( "#comic_description" ).append( "<p>"+item.description+"</p>" );
         if ( i === 0 ) {
           return false;
         }
